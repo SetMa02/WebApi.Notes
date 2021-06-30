bda1207307c5ac2ebd11b9fe7ba970b136a9c979
SetMa02 <maxuser99@mail.ru>
2021-06-30 18:02:15 +0700

    git init + add architecture

---
+++ Notes.Backend/Notes.Domain/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Notes.Domain
+{
+    public class Note
+    {
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public DateTime CreatingDate { get; set; }
+        public DateTime? EditDate { get; set; }
+        
+    }
+}
