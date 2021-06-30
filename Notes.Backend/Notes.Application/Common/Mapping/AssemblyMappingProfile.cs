d6da1487c5408ab763922f44c431792b2d3b3921
SetMa02 <maxuser99@mail.ru>
2021-06-30 20:48:10 +0700

    add mapping

---
+++ Notes.Backend/Notes.Application/Common/Mapping/AssemblyMappingProfile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using AutoMapper;
+using System.Reflection;
+
+namespace Notes.Application.Common.Mapping
+{
+    public class AssemblyMappingProfile : Profile
+    {
+        public AssemblyMappingProfile(Assembly assembly) =>
+            ApplyMappingFromAssembly(assembly);
+
+        private void ApplyMappingFromAssembly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes()
+                .Where(type => type.GetInterfaces()
+                    .Any(i => i.IsGenericType &&
+                              i.GetGenericTypeDefinition() == typeof(IMapWith<>)))
+                .ToList();
+
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new object[] {this});
+            }
+        }
+    }
+}
