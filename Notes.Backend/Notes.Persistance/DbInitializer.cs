31c5039272335e8d80a67c548785ece25756bdda
SetMa02 <maxuser99@mail.ru>
2021-06-30 18:42:35 +0700

    db added

---
+++ Notes.Backend/Notes.Persistance/DbInitializer.cs
@@ -0,0 +1,10 @@
+﻿namespace Notes.Persistance
+{
+    public class DbInitializer
+    {
+        public static void Initialized(NotesDbContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
---
+++ Notes.Backend/Notes.Persistance/NotesDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Application.Interfaces;
+using Notes.Domain;
+using Notes.Persistance.EntityTypeConfigurations;
+
+namespace Notes.Persistance
+{
+    public class NotesDbContext : DbContext , INoteDbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+        
+        public NotesDbContext(DbContextOptions<NotesDbContext> options)
+            : base(options){}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new NoteConfiguration());
+            base.OnModelCreating(builder);
+        }
+    }
+}
