aefa85896594a66b7b39237ea95f024246a74bd0
SetMa02 <maxuser99@mail.ru>
2021-07-01 01:29:51 +0700

    GetList Note added

---
+++ Notes.Backend/Notes.Application/Notes/Queries/GetNoteList/GetNoteListQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Notes.Application.Interfaces;
+using Notes.Application.Notes.Queries.GetNoteDetails;
+
+namespace Notes.Application.Notes.Queries.GetNoteList
+{
+    public class GetNoteListQueryHandler : IRequestHandler<GetNoteListQuery, NoteListVm>
+    {
+        private readonly INoteDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetNoteListQueryHandler(INoteDbContext dbContext, IMapper mapper) =>
+            (_dbContext, _mapper) = (dbContext, mapper);
+
+        public async Task<NoteListVm> Handle(GetNoteListQuery request, CancellationToken cancellationToken)
+        {
+            var notesQuery = await _dbContext.Notes
+                .Where(note => note.UserId == request.UserId)
+                .ProjectTo<NoteLookupDto>(_mapper.ConfigurationProvider)
+                .ToListAsync();
+            return new NoteListVm{Notes = notesQuery};
+        }
+    }
+}
---
+++ Notes.Backend/Notes.Application/Notes/Queries/GetNoteList/NoteLookupDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AutoMapper;
+using Notes.Application.Common.Mapping;
+using Notes.Domain;
+
+namespace Notes.Application.Notes.Queries.GetNoteList
+{
+    public class NoteLookupDto : IMapWith<Note>
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Note, NoteLookupDto>()
+                .ForMember(noteDto => noteDto.Id,
+                    opt => opt.MapFrom(note => note.Id))
+                .ForMember(noteDto => noteDto.Title,
+                    opt => opt.MapFrom(note => note.Title));
+        }
+    }
+}
