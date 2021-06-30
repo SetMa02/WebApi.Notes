429d0191b2e695561a16330500a86e4b443180f7
SetMa02 <maxuser99@mail.ru>
2021-07-01 01:09:15 +0700

    Get Note added

---
+++ Notes.Backend/Notes.Application/Notes/Queries/GetNoteDetails/GetNoteDetailsQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Notes.Application.Common.Exeptions;
+using Notes.Application.Interfaces;
+using Notes.Domain;
+
+namespace Notes.Application.Notes.Queries.GetNoteDetails
+{
+    public class GetNoteDetailsQueryHandler : IRequestHandler<GetNoteDetailsQuery, NoteDetailsVm>
+    {
+        private readonly INoteDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetNoteDetailsQueryHandler(INoteDbContext dbContext, IMapper mapper) =>
+            (_dbContext, _mapper) = (dbContext, mapper);
+        
+        public async Task<NoteDetailsVm> Handle(GetNoteDetailsQuery request, CancellationToken cancellationToken)
+        {
+            var entity = await _dbContext.Notes.FirstOrDefaultAsync(note =>
+                note.Id == request.Id, cancellationToken);
+
+            if (entity == null || entity.UserId != request.Id)
+            {
+                throw new NotFoundException(nameof(Note), request.Id);
+            }
+
+            return _mapper.Map<NoteDetailsVm>(entity);
+        }
+    }
+}
