ecdd9203472809737b7400ff7970f03fb01b1970
SetMa02 <maxuser99@mail.ru>
2021-07-01 00:28:28 +0700

    Update Note added

---
+++ Notes.Backend/Notes.Application/Common/Exeptions/NotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Notes.Application.Common.Exeptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string name, object key)
+            : base($"Entity \"{name}\" ({key}) not found")
+        {
+            
+        }
+      
+    }
+}
---
+++ Notes.Backend/Notes.Application/Notes/Commands/UpdateNote/UpdateNoteCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Notes.Application.Common.Exeptions;
+using Notes.Application.Interfaces;
+using Notes.Domain;
+
+namespace Notes.Application.Notes.Commands.UpdateNote
+{
+    public class UpdateNoteCommandHandler : IRequestHandler<UpdateNoteCommand>
+    {
+        private readonly INoteDbContext _dbContext;
+
+        public UpdateNoteCommandHandler(INoteDbContext dbContext) => _dbContext = dbContext;
+        
+        public async Task<Unit> Handle(UpdateNoteCommand request, CancellationToken cancellationToken)
+        {
+            var entity =
+                await _dbContext.Notes.FirstOrDefaultAsync(note => 
+                    note.Id == request.Id, cancellationToken);
+            if (entity == null || entity.UserId != request.UserId)
+            {
+                throw new NotFoundException(nameof(Note), request.Id);
+            }
+
+            entity.Details = request.Details;
+            entity.Title = request.Title;
+            entity.EditDate = DateTime.Now;
+            
+            return  Unit.Value;
+        }
+    }
+}
