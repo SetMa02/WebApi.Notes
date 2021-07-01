dd0ff6796cb26cc562b6d8a594957ad5323c542b
SetMa02 <maxuser99@mail.ru>
2021-07-01 17:30:48 +0700

    Dependency added

---
+++ Notes.Backend/Notes.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Notes.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAplication(
+            this IServiceCollection service)
+        {
+            service.AddMediatR(Assembly.GetExecutingAssembly());
+            return service;
+        }
+    }
+}
