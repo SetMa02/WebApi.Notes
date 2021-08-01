67d07edd68604968e88c709264b07af26e4845c2
Максим Майнагашев <maxuser99@mail.ru>
2021-08-02 00:17:46 +0700

    xUnit test for created note added

---
+++ Notes.Backend/Notes.Tests/Common/NotesContextFactory.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Notes.Domain;
+using Notes.Persistance;
+namespace Notes.Tests.Common
+{
+    public class NotesContextFactory
+    {
+        public static Guid UserAId = Guid.NewGuid();
+        public static Guid UserBId = Guid.NewGuid();
+        
+        public static Guid NotesIdForDelete = Guid.NewGuid();
+        public static Guid NotesIdForUpdate = Guid.NewGuid();
+
+        public static NotesDbContext Create()
+        {
+            var options = new DbContextOptionsBuilder<NotesDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            var context = new NotesDbContext(options);
+            context.Database.EnsureCreated();
+            context.AddRange(
+                new Note
+                {
+                    CreatingDate = DateTime.Today,
+                    Details = "Details1",
+                    EditDate = null,
+                    Id = Guid.Parse("09893E42-EB1B-4BA6-B79D-9B47DEB18E05"),
+                    Title = "Title1",
+                    UserId = UserAId
+                },
+                new Note
+                {
+                    CreatingDate = DateTime.Today,
+                    Details = "Details2",
+                    EditDate = null,
+                    Id = Guid.Parse("{337312C6-57E7-4026-986D-44D32B21953F}"),
+                    Title = "Title2",
+                    UserId = UserBId
+                },
+                new Note
+                {
+                    CreatingDate = DateTime.Today,
+                    Details = "Details3",
+                    EditDate = null,
+                    Id = NotesIdForDelete,
+                    Title = "Title3",
+                    UserId = UserAId
+                },
+                new Note
+                {
+                    CreatingDate = DateTime.Today,
+                    Details = "Details4",
+                    EditDate = null,
+                    Id = NotesIdForUpdate,
+                    Title = "Title4",
+                    UserId = UserBId
+                }   
+                );
+            context.SaveChanges();
+            return context;
+        }
+
+        public static void Destroy(NotesDbContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Dispose();
+        }
+        
+    }
+}
---
+++ Notes.Backend/Notes.Tests/Notes/Commands/DeleteNoteCommandHandlerTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Notes.Application.Common.Exeptions;
+using Notes.Application.Notes.Commands.CreateNote;
+using Notes.Application.Notes.Commands.DeleteCommand;
+using Notes.Tests.Common;
+using Xunit;
+
+namespace Notes.Tests.Notes.Commands
+{
+    public class DeleteNoteCommandHandlerTests : TestCommandBase
+    {
+        [Fact]
+        public async Task DeleteNoteCommandHandler_Success()
+        {
+            //Arrange
+            var handler = new DeleteNoteCommandHandler(Context);
+            
+            //Act
+            await handler.Handle(new DeleteNoteCommand
+            {
+                Id = NotesContextFactory.NotesIdForDelete,
+                UserId = NotesContextFactory.UserAId
+            }, CancellationToken.None);
+            
+            //Assert
+            Assert.Null(Context.Notes.SingleOrDefault(note => 
+                note.Id == NotesContextFactory.NotesIdForDelete));
+        }
+
+        [Fact]
+        public async Task DeleteNoteCommandHandler_FailOnWrongId()
+        {
+            //Arrange
+            var handler = new DeleteNoteCommandHandler(Context);
+
+            //Act
+
+            //Assert
+            await Assert.ThrowsAsync<NotFoundException>(async () =>
+                await handler.Handle(
+                    new DeleteNoteCommand
+                    {
+                        Id = Guid.NewGuid(),
+                        UserId = NotesContextFactory.UserAId
+                    }, CancellationToken.None));
+        }
+        
+        [Fact]
+        public async Task DeleteNoteCommandHandler_FailOnWrongUserId()
+        {
+            //Arrange
+            var deleteHandler = new DeleteNoteCommandHandler(Context);
+            var createHandler = new CreateNoteCommandHandler(Context);
+            var noteId = await createHandler.Handle(
+                new CreateNoteCommand
+                {
+                    Title = "Note title",
+                    UserId = NotesContextFactory.UserAId
+                }, CancellationToken.None);
+
+            //Act
+
+            //Assert
+            await Assert.ThrowsAsync<NotFoundException>(async () =>
+                await handler.Handle(
+                    new DeleteNoteCommand
+                    {
+                        Id = Guid.NewGuid(),
+                        UserId = NotesContextFactory.UserAId
+                    }, CancellationToken.None));
+        }
+    }
+}
